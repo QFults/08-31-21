3ccbdcf3ddc754dce41fa8f6e4dc2f14788ad5b0
Quinton Fults <quinton.fults.work@gmail.com>
2021-08-31 13:26:11 -0700

    code

---
+++ app.js
@@ -0,0 +1,12 @@
+// const chalk = require('chalk')
+
+// console.log(chalk.blue('Hello world!'))
+// console.log(chalk.bgWhite('What'))
+
+// const axios = require('axios')
+
+// axios.get('https://swapi.dev/api/people/1')
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => console.log(err))
